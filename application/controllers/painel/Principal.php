50a1b8613b863134754e7c7eff7205489f0df14f
hsystem <hlm.projetos@gmail.com>
2017-12-13 16:31:03 -0200

    fazendo testes 1

--- application/controllers/painel/Principal.php
+++ application/controllers/painel/Principal.php
@@ -1,6 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Principal extends MY_Controller {
+class Principal extends CI_Controller {
     
     public function __construct()
     {
